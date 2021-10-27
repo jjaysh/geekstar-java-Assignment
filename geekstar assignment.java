fd5d7b42e550f19ecda136a757aa541d78639915
jjaysh <93006422+jjaysh@users.noreply.github.com>
2021-10-27 19:17:21 +0530

    Add files via upload

---
+++ geekstar assignment.java
@@ -0,0 +1,15 @@
+geekstar assignment
+
+public class Hello_World
+{
+   Run Debug
+   public static void main(String[] args) 
+   {
+       
+   System.out.print1n("HELLO_WORLD");
+       
+   }
+
+
+
+}
