d39b9b0708d2dcd52d89cc002a030b12e078452e
Rohan Mane <63719271+RohanRMane@users.noreply.github.com>
2021-11-15 17:29:07 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,8 +1,6 @@
 class pattern
 {
-  
-        
-        public static void main(String[] args) {
+   public static void main(String[] args) {
             int n=5;
             for(int i=0;i<=n;i++)
             {
