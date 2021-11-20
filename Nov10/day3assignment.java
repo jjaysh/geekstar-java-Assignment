ae4c9ed90f519802cc6f4a05b68a306a8aa0d2c3
Rohan Mane <63719271+RohanRMane@users.noreply.github.com>
2021-11-20 17:08:38 +0530

    Create day3assignment.java

---
+++ Nov10/day3assignment.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+public class day3assignment
+ {
+
+    public static void main(String[] args) 
+    {
+     Scanner sc = new Scanner(System.in);
+        // how to declear byte datatype :
+        System.out.print("Enter the Byte = ");
+        byte b = sc.nextByte();
+        System.out.println("The byte value is : " + b);
+
+        // how to declear short value :
+        System.out.print("Enter the Short = ");
+        short a =sc.nextShort();
+        System.out.println("The short value is : " + a);
+
+        // how to declear integer value :
+        System.out.print("Enter the Integre = ");
+        int x = sc.nextInt();
+        System.out.println("The integer value is : " + x);
+
+        // how to declear long value :
+        System.out.print("Enter the Long no = ");
+        long l = sc.nextLong();
+        System.out.println("The long value is : " + l);
+
+        // how to declear float datatype :
+        System.out.print("Enter the Float no = ");
+        float f =sc.nextFloat();
+        System.out.println("The float value is : " + f);
+
+        // how to declear double datatype :
+        System.out.print("Enter the Double no = ");
+        double d =sc.nextDouble();
+        System.out.println("The double value is : " + d);
+
+        // how to declear char datatype :
+
+        char ch = 'R';
+        System.out.println("The character value is : " + ch);
+
+        // how to declear String datatype :
+
+        String str =sc.next();
+        System.out.println("The String value is : " + str);
+
+        // how to declear boolean datatype :
+
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println("The boolean value is : " + b1);
+        System.out.println("The boolean value is : " + b2);
+
+
+    }
+
+}
