c3ff538a1688ce209097c93816c6ad6b3a2638e1
Rohan Mane <63719271+RohanRMane@users.noreply.github.com>
2021-11-20 17:13:09 +0530

    Create nov14homework.java

---
+++ Nov14/nov14homework.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class nov14homework //ASCII value of any given character 
+{
+
+	public static void main(String args[])
+	{
+		@SuppressWarnings("resource")
+		Scanner sc=new Scanner(System.in);
+		char c;
+		System.out.print("Enter the character :");
+		c=sc.next().charAt(0);
+		int ch=(int)c;
+		System.out.println(ch);
+	
+    {
+        char a;
+		int b,s;
+		System.out.print("Enter the character :");
+		a=sc.next().charAt(0);
+		b=(int)a;
+		s=b+3;
+		System.out.println((char)s);
+    }
+    
+    {
+        double e,d;
+		System.out.print("Enter the temperature in degree Fahrenheit:");
+		e=sc.nextDouble();
+		d=((e-32)*5)/9;
+		System.out.println(d);
+    }
+    {
+        
+        double i,m;
+		System.out.println("Enter the length in inches:");
+		i=sc.nextDouble();
+	    m=i*0.0254;
+		System.out.println(m+"m");
+    }
+
+
+    System.out.print(" program to print the product of the decimal and int numbers");
+    double n1,pro;
+		int n2;
+		System.out.println("Enter the decimal number :");
+		n1=sc.nextDouble();
+		System.out.println("Enter the integer number :");
+		n2=sc.nextInt();
+		pro=n1*n2;
+		//double p=Math.round(pro);
+		System.out.println(n1+"*"+n2+"="+pro);
+    
+    }
+}
