632cd7d404d19498c1e4de0487b3d00d27429e5e
Rohan Mane <63719271+RohanRMane@users.noreply.github.com>
2022-01-02 16:55:59 +0530

    Add files via upload

---
+++ Dec 29/recbinarysearch.java
@@ -0,0 +1,34 @@
+public class recbinarysearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int arr[] = {11,24,36,44,53};  
+        int key = 36;  
+        int last=arr.length-1;  
+        int result = binarySearch(arr,0,last,key);  
+        if (result == -1) {
+            System.out.println("Element is not found!");
+        }else { 
+            System.out.println("Element is found at index: "+result); }
+	}
+	
+	  public static int binarySearch(int arr[], int first, int last, int key){ 
+		  
+	        if (last>=first){  
+	            int mid = first + (last - first)/2;  
+	            if (arr[mid] == key){  
+	            return mid;  
+	            }  
+	            
+	            if (arr[mid] > key){  
+	            return binarySearch(arr, first, mid-1, key);//search in left subarray  
+	            }else{  
+	            return binarySearch(arr, mid+1, last, key);//search in right subarray  
+	            }  
+	        }  
+	        return -1;  
+	    }  
+
+}
